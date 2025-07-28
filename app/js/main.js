721a31b5a3c09cd8cc9ae94cf446806de2ff9a24
AnnaKoha <kohovich.anna@gmail.com>
2025-07-28 16:01:01 +0300

    Initial commit: uploaded site practicing Bento layout, responsive design, Gulp, and SCSS

---
+++ app/js/main.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.projects__slider', {
+   
+
+  effect: "fade",
+  fadeEffect: {
+    crossFade: true
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+
+});
